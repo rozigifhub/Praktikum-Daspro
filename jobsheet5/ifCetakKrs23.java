19bef0eafe6e8a7be31bd77e158f210f9c6b0e20
rozigifhub <rozigming@gmail.com>
2025-09-30 12:07:00 +0700

    Modifikasi 1 penambahan else

--- jobsheet5/ifCetakKrs23.java
+++ jobsheet5/ifCetakKrs23.java
@@ -11,6 +11,8 @@ class ifCetakKrs23 {
         if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }else{
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
 
         sc.close();
