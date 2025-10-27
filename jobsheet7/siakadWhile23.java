6dc216465cd6523c1d5ac92fca8a49112b17d589
rozigifhub <rozigming@gmail.com>
2025-10-27 10:37:42 +0700

    modifikasi percobaan 2

--- jobsheet7/siakadWhile23.java
+++ jobsheet7/siakadWhile23.java
@@ -21,6 +21,7 @@ public class siakadWhile23 {
             // Menentukan kategori huruf
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai huruf: A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai huruf: B+");
             } else if (nilai > 65 && nilai <= 73) {
