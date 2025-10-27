e6bec1f581a1ae2074862dc417395302f64bd551
rozigifhub <rozigming@gmail.com>
2025-10-27 10:29:16 +0700

    modifikasi percobaann 1

--- jobsheet7/siakadFor23.java
+++ jobsheet7/siakadFor23.java
@@ -4,12 +4,14 @@ public class siakadFor23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         // Input nilai 10 mahasiswa
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
 
+            // Cek nilai tertinggi dan terendah
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
@@ -17,11 +19,21 @@ public class siakadFor23 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // Hitung jumlah lulus dan tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
+        // Tampilkan hasil
         System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+
         sc.close();
     }
 }
-
