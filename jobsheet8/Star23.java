d752f30de52da577b20d3ca090dc54b61abfc672
rozigifhub <rozigming@gmail.com>
2025-10-29 13:38:08 +0700

    percobaan 1

---
+++ jobsheet8/Star23.java
@@ -0,0 +1,15 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Star23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        for(int i=1; i > n; i++){
+            System.out.print("*");
+        }
+    }
+}
