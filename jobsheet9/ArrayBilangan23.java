c153bd59aa70cf5cb6dde3da69c11ab9df1a30e1
rozigifhub <rozigming@gmail.com>
2025-11-04 10:26:33 +0700

    Percobaan 1

---
+++ jobsheet9/ArrayBilangan23.java
@@ -0,0 +1,20 @@
+package jobsheet9;
+
+public class ArrayBilangan23 {
+    public static void main(String[] args) {
+        // Membuat array dengan 4 elemen
+        int[] bil = new int[4];
+
+        // Mengisi elemen array
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        // Menampilkan isi elemen array
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
