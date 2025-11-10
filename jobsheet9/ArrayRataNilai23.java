8737b20500b1d5bb466d38d7fcbcdf953eeba6e1
rozigifhub <rozigming@gmail.com>
2025-11-10 11:42:37 +0700

    modifikasi percobaan 3

--- jobsheet9/ArrayRataNilai23.java
+++ jobsheet9/ArrayRataNilai23.java
@@ -8,11 +8,18 @@ public class ArrayRataNilai23 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jmlLulus = 0;
+
+        System.out.println("masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
 
         // Input nilai ke dalam array
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < jmlMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70){
+                jmlLulus += 1;
+            }
         }
 
         // Menghitung total
@@ -23,6 +30,8 @@ public class ArrayRataNilai23 {
         // Menghitung rata-rata
         rata2 = total / nilaiMhs.length;
 
+        
+        System.out.println("Jumlah Lulus: " + jmlLulus);
         System.out.println("Rata-rata nilai mahasiswa = " + rata2);
     }
 }
